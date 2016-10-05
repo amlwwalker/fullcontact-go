c9b3913b203aff0da223fedf7afe185b4b68527d
Alex Walker <amlwwalker@gmail.com>
2016-10-05 13:54:40 +0100

    changing social profile to string because of flickr

--- person.go
+++ person.go
@@ -192,7 +192,7 @@ type LocationPart struct {
 
 // SocialProfile is a sub-model of PersonResponse
 type SocialProfile struct {
-	ID        int64  `json:"id,string"`
+	ID        string  `json:"id,string"`
 	Type      string `json:"type"`
 	TypeID    string `json:"typeId"`
 	TypeName  string `json:"typeName"`
